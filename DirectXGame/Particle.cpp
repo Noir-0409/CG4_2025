b4df732b6483a94a52e125c2e4118813b72eca2f
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-05-07 14:25:30 +0900

    色の変更

--- DirectXGame/Particle.cpp
+++ DirectXGame/Particle.cpp
@@ -8,6 +8,10 @@ void Particle::Initialize(Model* model) {
 
 	worldTransform_.Initialize();
 
+	//色の設定
+	objectColor_.Initialize();
+	color_ = {1, 1, 0, 1};
+
 }
 
 void Particle::Update() {
@@ -15,10 +19,14 @@ void Particle::Update() {
 //行列を定数バッファに転送
 	worldTransform_.TransferMatrix();
 
+//色変更オブジェクトに色の数値を設定
+	objectColor_.SetColor(color_);
+
 }
 
 void Particle::Draw(Camera& camera) {
 
-	model_->Draw(worldTransform_, camera);
+	model_->Draw(worldTransform_, camera, &objectColor_);
+
 
 }
